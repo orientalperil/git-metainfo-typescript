c62f9fe6d950760b5dd91d710332ade533fa8f60
Kevin Tran <151680+orientalperil@users.noreply.github.com>
2026-05-08 22:35:57 -0700

    ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,37 @@
-import js from "@eslint/js";
-import prettier from "eslint-config-prettier";
-import tsParser from "@typescript-eslint/parser";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
+import js from "@eslint/js"
+import tsPlugin from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+import prettier from "eslint-config-prettier"
+import globals from "globals"
 
 export default [
-  js.configs.recommended,
-  prettier,
   {
-    ignores: ["bin/git-metainfo.js", "dist/**"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+
+  js.configs.recommended,
+
+  prettier,
+
+  {
     files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
       parser: tsParser,
       ecmaVersion: "latest",
       sourceType: "module",
       globals: {
-        process: "readonly",
-        module: "readonly",
-        require: "readonly",
-        __dirname: "readonly",
+        ...globals.node,
       },
     },
     plugins: {
       "@typescript-eslint": tsPlugin,
     },
+    rules: {
+      "no-undef": "off",
+    },
   },
-];
+]
